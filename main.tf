509a35dc9ebb693acb791f15e9b7cf21c11b8f0d
unixgnuru-svg <84354424+unixgnuru-svg@users.noreply.github.com>
2025-09-11 14:26:19 -0400

    fixed a "

--- main.tf
+++ main.tf
@@ -11,12 +11,12 @@ provider "aws" {
        
 }
 
-resource "aws_instance" "example{
+resource "aws_instance" "example"{
     ami = "ami-0b09ffb6d8b58ca91"
     instance_type = "t2.micro"
     tags = {
-        Name = "EXAMPLE
+        Name = "EXAMPLE"
         ENV = "DEV"
         Project = "Terraform"
     }
-}"
+}
